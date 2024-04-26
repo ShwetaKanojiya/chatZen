41b8d17112cfdd95de3bd3d2d6650a9810674b96
abc <shwetahimkanojiya@example.com>
2024-04-26 14:41:42 +0530

    Remove variable abc

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -9,7 +9,6 @@ import { useHistory } from "react-router-dom";
 import { ChatState } from "../../Context/ChatProvider";
 
 const Login = () => {
-  const abc = 1;
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
   const toast = useToast();
